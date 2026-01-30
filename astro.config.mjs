a48176fc3741158b191391ad677cd08b67613257
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2026-01-30 13:32:03 +0530

    dev serve at 3000

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   integrations: [react()],
 
   vite: {
