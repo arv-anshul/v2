38fd1de5d4975d3f062f2ca7d19b13c45e704973
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2026-01-31 10:54:48 +0530

    add /projects

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,19 +3,32 @@ import { glob } from "astro/loaders";
 
 // TODO: Sync this with Astro 6
 // import { iconSchema } from "@/lib/data-loaders/common";
+const iconSchema = z
+  .string()
+  .regex(/^(lucide:|simple-icons:)/)
+  .toLowerCase();
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "content/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    icon: z
-      .string()
-      .regex(/^(lucide:|simple-icons:)/)
-      .toLowerCase(),
+    icon: iconSchema,
     author: z.enum(["Anshul Raj Verma"] as const),
     date: z.date(),
   }),
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "content/projects" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // TODO: Use z.httpUrl
+    projectUrl: z.string().optional(),
+    date: z.date().optional(),
+    icon: iconSchema.optional(),
+  }),
+});
+
+export const collections = { blog, projects };
