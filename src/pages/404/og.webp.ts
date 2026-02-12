438e1382af469c5220026d265afb3b5a7a751fab
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2026-02-13 00:41:09 +0530

    show 404 og image for unavailable pages

--- src/pages/404/og.webp.ts
+++ src/pages/404/og.webp.ts
@@ -5,19 +5,17 @@ import {
 } from "@/lib/og";
 
 const imageData: OgImageData = {
-  title: "404 - ARV",
+  title: "404: Not Found",
   description:
     "The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.",
   icon: "mdi:skull-crossbones",
 };
 
-export const GET = async ({
-  request,
-}: {
-  props: OgImageData;
-  request: Request;
-}) => {
-  const markup = await getMarkupFromOgData(request.url, imageData);
+export const GET = async ({ request }: { request: Request }) => {
+  const markup = await getMarkupFromOgData(
+    new URL(request.url).origin,
+    imageData
+  );
   const imageBuffer = await getImageBufferFromMarkup(markup);
   return new Response(imageBuffer);
 };
